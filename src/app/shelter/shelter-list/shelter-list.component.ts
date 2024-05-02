e5bf5225c2c49d83a61828f5cc224e676f3cdca5
LordDangerous <mamrayo10@gmail.com>
2024-05-02 15:16:48 +0200

    Initial angular materials for the project

---
+++ src/app/shelter/shelter-list/shelter-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shelter-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './shelter-list.component.html',
+})
+export class ShelterListComponent {
+
+}
