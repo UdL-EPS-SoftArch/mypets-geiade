06795772d8074909cab8718541116762f7431d00
LordDangerous <mamrayo10@gmail.com>
2024-05-08 17:56:13 +0200

    Created shelter.ts and service

--- src/app/shelter/shelter-list/shelter-list.component.ts
+++ src/app/shelter/shelter-list/shelter-list.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {Shelter} from "../shelter";
+import {Router} from "@angular/router";
+import {ShelterService} from "../shelter.service";
+import {AuthenticationBasicService} from "../../login-basic/authentication-basic.service";
 
 @Component({
   selector: 'app-shelter-list',
@@ -7,5 +11,17 @@ import { Component } from '@angular/core';
   templateUrl: './shelter-list.component.html',
 })
 export class ShelterListComponent {
+  public shelters: Shelter[] = [];
+  public pageSize = 5;
+  public page = 1;
+  public totalSeeds = 0;
+  public sortBy = 'Scientific name';
+  public sortOrder = 'A-Z';
+
+  constructor(
+    public router: Router,
+    private shelterService: ShelterService,
+    private authenticationService: AuthenticationBasicService
+  ) {}
 
 }
