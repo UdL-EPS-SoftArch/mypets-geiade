f5482d5a9c3110d599f95fe837091205dbf26398
Hdelegido <hectordelegido2001@gmail.com>
2024-05-14 15:50:35 +0200

    role-list.component.html and role-list.component.ts created

---
+++ src/app/role/role-list/role-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-role-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './role-list.component.html',
+})
+export class RoleListComponent {
+
+}
