d7c930f1d29c9689aa34bb55d6f717fc492036b0
PauMAVA <paumachetti@gmail.com>
2024-05-18 14:56:42 +0200

    Add dog and cat lists

---
+++ src/app/pet/dog/dog-list/dog-list.component.ts
@@ -0,0 +1,44 @@
+import {Component, OnInit} from '@angular/core';
+import {Dog} from "../dog";
+import {Router} from "@angular/router";
+import {DogService} from "../dog.service";
+import {PagedResourceCollection} from "@lagoshny/ngx-hateoas-client";
+
+@Component({
+  selector: 'app-dog-list',
+  templateUrl: './dog-list.component.html',
+})
+export class DogListComponent implements OnInit {
+  public dogs: Dog[] = [];
+  public pageSize = 5;
+  public page = 1;
+  public totalDogs = 0;
+
+  constructor(
+    public router: Router,
+    private dogService: DogService,
+  ) {}
+
+  ngOnInit(): void {
+    this.dogService
+      .getPage({
+        pageParams: { size: this.pageSize },
+        sort: { name: 'ASC' },
+      })
+      .subscribe((page: PagedResourceCollection<Dog>) => {
+        this.dogs = page.resources;
+        this.totalDogs = page.totalElements;
+      });
+  }
+
+  changePage(): void {
+    this.dogService
+      .getPage({
+        pageParams: { page: this.page - 1, size: this.pageSize },
+        sort: { name: 'ASC' },
+      })
+      .subscribe(
+        (page: PagedResourceCollection<Dog>) => (this.dogs = page.resources)
+      );
+  }
+}
